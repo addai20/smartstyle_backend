4e7061277aa8d28e55eb4d2dde84de2c37b63941
gillsinaglass <rdgill95@gmail.com>
2019-03-25 11:16:42 -0400

    Categories are created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_22_193451) do
+ActiveRecord::Schema.define(version: 2019_03_22_193551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_03_22_193451) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "low_temp"
+    t.integer "high_temp"
   end
 
   create_table "item_categories", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Category.destroy_all
+
+rain = Category.create(title: "Rain", description: "Gear for weather when raining")
+snow = Category.create(title: "Snow", description: "Gear for weather when snowing")
+sunny = Category.create(title: "Sunny", description: "Gear for weather when Sunny")
+very_cold = Category.create(title: "Very Cold", description: "Gear for weather when Very Cold")
+cold = Category.create(title: "Cold", description: "Gear for weather when Cold")
+moderate = Category.create(title: "Moderate", description: "Gear for weather when Moderate")
+warm = Category.create(title: "Warm", description: "Gear for weather when Warm")
+hot = Category.create(title: "Hot", description: "Gear for weather when Hot")
